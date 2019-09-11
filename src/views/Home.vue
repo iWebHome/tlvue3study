d3f3d1c88e9d29c37b2672e358121891c04a5388
lichuanjun <lichuanjun@niwodai.net>
2019-09-11 18:12:45 +0800

    组件 home.vue 中学习指令和绑定

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="home">
     <div class="nav">
-      <div v-for="(value, key, index) in navs" :key="index">
+      <div v-for="(value, key, index) in navs" :key="index" :class="{ active: value.active }" @click="changeNav(key)">
         {{key}}
       </div>
       <ul>
-        <li v-for="(tt, index) in tts" :key="index">
-          {{tt.title}}
+        <li v-for="(tt, index) in tts" :key="index" v-if="tt.title">
+          <div><a :href="tt.link">{{tt.title}}</a></div>
+          <span>{{tt.ptime}} - {{tt.source}}</span>
         </li>
       </ul>
     </div>
@@ -23,13 +24,27 @@ export default {
       tts: []
     }
   },
+  methods: {
+    changeNav: function (akey) {
+      Reflect.ownKeys(this.navs).forEach((key, index) => {
+        this.navs[key].active = akey === key && (this.tts = this.navs[key].data)
+      })
+    }
+  },
   created: function () {
     fetch('https://www.apiopen.top/journalismApi')
     .then(v=>v.json())
     .then(v=>{
-      console.log(v.data)
-      this.tts = v.data.toutiao
       this.navs = v.data
+      this.tts = v.data.toutiao
+      console.log(v.data)
+
+      Reflect.ownKeys(v.data).forEach((key, index) => {
+        this.navs[key] = {
+          active: key === 'toutiao',
+          data: this.navs[key]
+        }
+      })
     })
   }
 }
