5a4cfc2621febaa476f3f49137d912e0fe377f7b
lichuanjun <lichuanjun@niwodai.net>
2019-09-11 17:45:57 +0800

    组件 home.vue 的初步改造

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="nav">
+      <div v-for="(value, key, index) in navs" :key="index">
+        {{key}}
+      </div>
+      <ul>
+        <li v-for="(tt, index) in tts" :key="index">
+          {{tt.title}}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  data: function () {
+    return {
+      navs: {},
+      tts: []
+    }
+  },
+  created: function () {
+    fetch('https://www.apiopen.top/journalismApi')
+    .then(v=>v.json())
+    .then(v=>{
+      console.log(v.data)
+      this.tts = v.data.toutiao
+      this.navs = v.data
+    })
   }
 }
 </script>
