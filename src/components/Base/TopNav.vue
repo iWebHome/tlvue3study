ab29ea2813ba3f4df227dbf5689cfe5405ef496c
lichuanjun <lichuanjun@niwodai.net>
2019-09-11 20:38:53 +0800

    自定义一个导航栏组件，并在About.vue中使用

---
+++ src/components/Base/TopNav.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="topnav">
+    {{title}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'topnav',
+  props: ['title'],
+  data: function() {
+    return {
+      text: '导航条'
+    }
+  }
+}
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="about">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <top-nav title="推荐"/>
+    <top-nav title="军事"/>
+    <top-nav title="社会"/>
+    <top-nav title="科技"/>
     <HelloWorld msg="vue 官方相关资料的链接"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from '@/components/HelloWorld.vue'
+import TopNav from '@/components/Base/TopNav.vue'
 
 export default {
   name: 'about',
   components: {
-    HelloWorld
+    HelloWorld,
+    TopNav
   }
 }
 </script>
